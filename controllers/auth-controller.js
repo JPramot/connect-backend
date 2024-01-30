61548f4d4ab886d2bf431edcdad6462c2bef13a1
JPramot <jkurathong@gmail.com>
2024-01-30 09:34:10 +0700

    day-2

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,7 +1,10 @@
 const prisma = require("../models/prisma");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const tryCatch = (func) => (req, res, next) => func(req, res, next).catch(next);
+
+const secret = process.env.SECRET;
 
 module.exports.register = tryCatch(async (req, res, next) => {
   const { s_code, firstName, password, confirmPassword, email } = req.body;
@@ -19,4 +22,21 @@ module.exports.register = tryCatch(async (req, res, next) => {
   res.json({ message: "register success" });
 });
 
-module.exports.login = (req, res, next) => {};
+exports.login = tryCatch(async (req, res, next) => {
+  const { s_code, t_code, password } = req.body;
+  if (!((s_code || t_code) && password)) {
+    throw new Error("student code and password is required");
+  }
+  const student = await prisma.student.findFirst({
+    where: {
+      s_code: s_code,
+    },
+  });
+  const decode = await bcrypt.compare(password, student.password);
+  if (!decode) throw new Error("s_code or password wrong");
+  const token = jwt.sign({ id: student.id, s_code: student.s_code }, secret, {
+    expiresIn: 60 * 60 * 24,
+  });
+  const { password: pw, ...obj } = req.body;
+  res.status(200).json({ ...obj, token });
+});
